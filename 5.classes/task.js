cf6b1ac9eb4ad2fcf58f6efa7bb53e4e6b32a635
Ksenia Pletinka <pletinkaksenia@gmail.com>
2023-11-26 00:45:04 +0300

    Пятое задание полностью готово

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,25 +1,25 @@
-// Задача 1
-
+// 1 задача
 class PrintEditionItem {
+	_state = 100;
+	type = null;
+
 	constructor(name, releaseDate, pagesCount) {
 		this.name = name;
 		this.releaseDate = releaseDate;
 		this.pagesCount = pagesCount;
-		this.state = 100;
-		this.type = null;
 	}
 
 	fix() {
-		this.state *= 1.5;
+		this.state = this.state * 1.5;
 	}
 
-	set state(newState) {
-		if (newState < 0) {
-			this._state = 0;
-		} else if (newState > 100) {
+	set state(stateNubmer) {
+		if (stateNubmer > 100) {
 			this._state = 100;
+		} else if (this.state < 0) {
+			this._state = 0;
 		} else {
-			this._state = newState;
+			this._state = stateNubmer;
 		}
 	}
 
@@ -31,178 +31,107 @@ class PrintEditionItem {
 class Magazine extends PrintEditionItem {
 	constructor(name, releaseDate, pagesCount) {
 		super(name, releaseDate, pagesCount);
-		this.type = 'magazine';
 	}
+	type = "magazine";
 }
 
-class Book extends PrintEditionItem {
+class Book extends Magazine {
 	constructor(author, name, releaseDate, pagesCount) {
 		super(name, releaseDate, pagesCount);
 		this.author = author;
-		this.type = 'book';
 	}
+	type = "book";
 }
 
 class NovelBook extends Book {
-	constructor(author, name, releaseDate, pagesCount) {
-		super(author, name, releaseDate, pagesCount);
-		this.type = 'novel';
-	}
+	type = "novel";
 }
-
 class FantasticBook extends Book {
-	constructor(author, name, releaseDate, pagesCount) {
-		super(author, name, releaseDate, pagesCount);
-		this.type = 'fantastic';
-	}
+	type = "fantastic";
 }
-
 class DetectiveBook extends Book {
-	constructor(author, name, releaseDate, pagesCount) {
-		super(author, name, releaseDate, pagesCount);
-		this.type = 'detective';
-	}
+	type = "detective";
 }
 
-const sherlock = new PrintEditionItem(
-	"Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-	2019,
-	1008
-);
 
-console.log(sherlock.releaseDate);
-console.log(sherlock.state);
-sherlock.fix();
-console.log(sherlock.state);
-
-const picknick = new FantasticBook(
-	"Аркадий и Борис Стругацкие",
-	"Пикник на обочине",
-	1972,
-	168
-);
-
-console.log(picknick.author);
-picknick.state = 10;
-console.log(picknick.state);
-picknick.fix();
-console.log(picknick.state);
-
-
-
-// Задача 2
-
+// 2 задача
 class Library {
+	books = [];
+
 	constructor(name) {
 		this.name = name;
-		this.books = [];
 	}
 
 	addBook(book) {
-		if (book.state > 30) {
-			this.books.push(book);
-		}
+		(book.state > 30) ? this.books.push(book): this.books.push(null);
 	}
 
 	findBookBy(type, value) {
-		for (let i = 0; i < this.books.length; i++) {
-			if (this.books[i][type] === value) {
-				return this.books[i];
-			}
-		}
-		return null;
+		let bookIndex = this.books.findIndex((item) => item[type] === value);
+		return this.books[bookIndex] = (bookIndex != -1) ? this.books[bookIndex] : null;
 	}
 
 	giveBookByName(bookName) {
-		for (let i = 0; i < this.books.length; i++) {
-			if (this.books[i].name === bookName) {
-				const book = this.books[i];
-				this.books.splice(i, 1);
-				return book;
-			}
+		let bookIndex = this.books.findIndex((item) => item.name === bookName);
+		if (bookIndex != -1) {
+			let bookReturn = this.books[bookIndex];
+			this.books.splice([bookIndex], [1]);
+			return bookReturn;
+		} else {
+			return null;
 		}
-		return null;
 	}
 }
 
-const library = new Library("Библиотека имени Ленина");
-
-library.addBook(
-	new DetectiveBook(
-		"Артур Конан Дойл",
-		"Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-		2019,
-		1008
-	)
-);
-library.addBook(
-	new FantasticBook(
-		"Аркадий и Борис Стругацкие",
-		"Пикник на обочине",
-		1972,
-		168
-	)
-);
-library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
-library.addBook(new Magazine("Мурзилка", 1924, 60));
-
-console.log(library.findBookBy("name", "Властелин колец"));
-console.log(library.findBookBy("releaseDate", 1924).name);
-
-console.log("Количество книг до выдачи: " + library.books.length);
-library.giveBookByName("Машина времени");
-console.log("Количество книг после выдачи: " + library.books.length);
-
-// Задача 3
-
-
+// 3 задача
 class Student {
-	constructor(name) {
+	marks = {};
+
+	constructor(name, gender, age) {
 		this.name = name;
-		this.marks = {};
+		this.gender = gender;
+		this.age = age;
 	}
 
-	addMark(mark, subject) {
-		if (mark < 2 || mark > 5) {
-			console.log("Неверный формат оценки");
-			return;
+	addMark(mark, discipline) {
+		if (mark < 1 || mark > 5) {
+			return console.log('Ошибка, оценка должна быть числом от 1 до 5');
+		} else if (this.marks[discipline] != undefined) {
+			this.marks[discipline].push(mark);
+		} else {
+			this.addDiscipline(discipline);
+			this.marks[discipline].push(mark);
 		}
-
-		if (!this.marks[subject]) {
-			this.marks[subject] = [];
-		}
-
-		this.marks[subject].push(mark);
 	}
 
-	getAverageBySubject(subject) {
-		if (!this.marks[subject]) {
-			return 0;
-		}
+	addDiscipline(discipline) {
+		this.marks[discipline] = [];
+	}
 
-		const sum = this.marks[subject].reduce((acc, cur) => acc + cur, 0);
-		const average = sum / this.marks[subject].length;
-		return average;
+	getAverageBySubject(discipline) {
+		if (this.marks[discipline] != undefined || this.marks[discipline].length > 0) {
+			let sum = this.marks[discipline].reduce((acc, item) => acc += item);
+			return sum / this.marks[discipline].length
+		} else if (this.marks[discipline] != undefined || this.marks[discipline].length === 0) {
+			return console.log('оценок нет');
+		} else {
+			return console.log(`Предмета ${discipline} не существует`);
+		}
 	}
 
 	getAverage() {
-		const subjects = Object.keys(this.marks);
-		const totalSum = subjects.reduce((acc, subject) => {
-			const subjectAverage = this.getAverageBySubject(subject);
-			return acc + subjectAverage;
-		}, 0);
-		const totalAverage = totalSum / subjects.length;
-		return totalAverage;
+		let length = 0;
+		let sum = 0;
+		for (let key in this.marks) {
+			sum += this.marks[key].reduce((acc, item) => acc += item);
+			length += this.marks[key].length;
+		}
+		return sum / length
+	}
+
+	exclude(reason) {
+		delete this.marks;
+		this.excluded = reason;
+		console.log(reason);
 	}
 }
-
-const student = new Student("Олег Никифоров");
-student.addMark(5, "химия");
-student.addMark(5, "химия");
-student.addMark(5, "физика");
-student.addMark(4, "физика");
-student.addMark(6, "физика");
-
-console.log(student.getAverageBySubject("физика"));
-console.log(student.getAverageBySubject("биология"));
-console.log(student.getAverage());
