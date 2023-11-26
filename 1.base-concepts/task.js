4d3c7b73d13431b7d18e7d419b2e2863ab46e8aa
Ksenia Pletinka <pletinkaksenia@gmail.com>
2023-11-26 10:33:42 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,40 +1,36 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = (b ** 2) - (4 * a * c);
-  if (d < 0) {
-    arr.push();
-  } else if (d == 0) {
-    let oneRoot = -b / (2 * a);
-    arr.push(oneRoot);
-  } else if (d > 0) {
-    let firstRoot = (-b + Math.sqrt(d))/(2 * a);
-		let secondRoot = (-b - Math.sqrt(d))/(2 * a);
-			arr.push(firstRoot, secondRoot);
-  }
-  
-  return arr;
+	let arr = [];
+	let d = (b ** 2) - (4 * a * c);
+	if (d < 0) {
+		arr.push();
+	} else if (d == 0) {
+		let oneRoot = -b / (2 * a);
+		arr.push(oneRoot);
+	} else if (d > 0) {
+		let firstRoot = (-b + Math.sqrt(d)) / (2 * a);
+		let secondRoot = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(firstRoot, secondRoot);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let totalAmount;
-		if (isNaN(percent)) {
+	let totalAmount;
+	if (isNaN(percent)) {
 		return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
 	} else if (isNaN(contribution)) {
 		return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
 	} else if (isNaN(amount)) {
 		return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
-	} 	else if (isNaN(amount)) {
-		return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${countMonths}"`;
-  }
-	let returnAmountBank = amount - contribution;	
-	let date1 = new Date();	
+	}
+	let returnAmountBank = amount - contribution;
+	let date1 = new Date();
 	let diff = Date.parse(date) - Date.parse(date1);
-	let creditTerm = Math.ceil(diff / 1000 / 60 / 60 / 24 / 30.5);	
+	let creditTerm = Math.ceil(diff / 1000 / 60 / 60 / 24 / 30.5);
 	let P = percent / 12 / 100;
 	let monthlyFee = returnAmountBank * (P + (P / (((1 + P) ** creditTerm) - 1)));
-			totalAmount = creditTerm * monthlyFee;				
-    return Number(totalAmount.toFixed(2));
-  
+	totalAmount = creditTerm * monthlyFee;
+	return Number(totalAmount.toFixed(2));
 }
